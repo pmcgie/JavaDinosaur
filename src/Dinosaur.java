bae98a314826118786f04d45b1798f5508e4e645
pmm <paul@yahoo.com>
2017-02-15 17:31:47 -0600

    Added a few dinosaurs

---
+++ src/Dinosaur.java
@@ -0,0 +1,8 @@
+/**
+ * Created by pmcgie on 2/15/2017.
+ */
+public class Dinosaur {
+
+    public void roar(){
+        System.out.println("GRRR!!!!!");}
+}
---
+++ src/DinosaurSupervisor.java
@@ -0,0 +1,15 @@
+/**
+ * Created by pmcgie on 2/15/2017.
+ */
+public class DinosaurSupervisor {
+
+    public static void main(String[] args) {
+        Dinosaur t_rex = new Dinosaur();
+        Dinosaur velociraptor = new Dinosaur();
+        t_rex.roar();
+        velociraptor.roar();
+        Dinosaur stegosaurus = new Dinosaur();
+        stegosaurus.roar();
+        Dinosaur pterodactyl = new Dinosaur();
+    }
+}
